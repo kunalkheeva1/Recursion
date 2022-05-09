d30b77a16ddd206b7944479bd864f70f5975ca4c
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-05-08 18:25:09 -0700

    recursion with the given input with the maximum limit too

--- src/Recursion.java
+++ src/Recursion.java
@@ -1,17 +1,19 @@
 import java.util.Scanner;
 public class Recursion {
-    public static void print(int n){
-        if(n==0){
+    public static void print(int n, int m){
+        if(n==m+1){
             return;
         }
         System.out.print(n+ " ");
-            print(n-1);
+            print(n+1,m);  //added the maximum limit in the recursion with anther parameter m as input.
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Please enter your input to be reversed: ");
+        System.out.println("Please enter your input to get the ascending list: ");
         int input =sc.nextInt();
-        print(input);
+        System.out.println("Please enter your maximum limit: ");
+        int input2= sc.nextInt();
+        print(input,input2);
 
     }
 }
