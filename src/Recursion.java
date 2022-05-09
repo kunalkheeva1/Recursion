9e2da6886059bad14a6045b17d64791949096496
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-05-08 18:25:09 -0700

    recursion with the given input!!

--- src/Recursion.java
+++ src/Recursion.java
@@ -4,8 +4,8 @@ public class Recursion {
         if(n==0){
             return;
         }
-        System.out.print(n+ " ");
-            print(n-1);
+        System.out.print(n-1+ " ");
+            print(n-2);
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
