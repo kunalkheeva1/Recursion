bb4fbaff686a625d6ef4551b15e78bcb6af21fd4
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-05-08 17:28:19 -0700

    Initial commit

---
+++ src/Recursion.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Recursion {
+    public static void print(int n){
+        if(n==0){
+            return;
+        }else{
+            System.out.println(n);
+            print(n-1);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int in
+
+    }
+}
