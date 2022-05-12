0340fc4a861bae1a4fe331dcc55ef635a7c02185
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-05-11 19:49:48 -0700

    check the occurance of the elements

--- src/Recursion.java
+++ src/Recursion.java
@@ -1,15 +1,26 @@
 public class Recursion {
-    public static void reverse(String str, int idx){
-        if(idx==0){
-            System.out.println(str.charAt(idx));
+    static int first =-1;
+    static int last = -1;
+    public static void occurance(String str, int idx, char element){
+        if(idx==str.length()){
+            System.out.println(first);
+            System.out.println(last);
             return;
         }
-        System.out.print (str.charAt(idx));
-        reverse(str,idx-1);
+
+        char current= str.charAt(idx);
+        if(current== element){
+            if(first==-1){
+                first =idx;
+            }else{
+                last=idx;
+            }
+        }
+        occurance(str,idx+1,element);
     }
     public static void main(String[] args) {
-        String kk= "Kunal";
-        reverse(kk,4);
+        occurance("Kunaaal",0,'a');
+
 
     }
 }
