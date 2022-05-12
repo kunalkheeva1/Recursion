077e0ee1e98ab5abde3cc39d62cde2f474aa5a7e
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-05-11 19:30:42 -0700

    recursion of printing name reversely

--- src/Recursion.java
+++ src/Recursion.java
@@ -1,17 +1,15 @@
 public class Recursion {
-    public static void towerOfHanoi(int n, String src, String helper, String dest){
-    if(n==1){
-        System.out.println("Transfer of disk " +n +" from "+ src +" to "+dest);
-        return;
-    }
-
-        towerOfHanoi(n-1,src,dest,helper);
-        System.out.println("Transfer of n disk "+n+" from " +src+" to "+ dest);
-        towerOfHanoi(n-1,helper,src,dest);
-
+    public static void reverse(String str, int idx){
+        if(idx==0){
+            System.out.println(str.charAt(idx));
+            return;
+        }
+        System.out.print (str.charAt(idx));
+        reverse(str,idx-1);
     }
     public static void main(String[] args) {
-        towerOfHanoi(5,"S","H","D");
+        String kk= "Kunal";
+        reverse(kk,4);
 
     }
 }
